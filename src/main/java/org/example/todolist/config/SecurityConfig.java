828841a706441e4037b0f307a041730a7aa5dca1
Chan-GN <ocg0229@gmail.com>
2024-09-11 14:59:58 +0900

    REFACTOR: 패키지 이름 변경(security -> config)

--- src/main/java/org/example/todolist/config/SecurityConfig.java
+++ src/main/java/org/example/todolist/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package org.example.todolist.security;
+package org.example.todolist.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
