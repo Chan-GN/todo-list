bffb6dc2b0c57c8bcda621f8bcc2e94494a08320
Chan-GN <ocg0229@gmail.com>
2024-09-11 15:00:52 +0900

    FEAT: CustomUserDetalisService 구현
    - CustomUserDetalis 추가
    - MeberRepository 추가
    - findByLoginId 메서드 추가
    - CustomUserDetalisService 추가

---
+++ src/main/java/org/example/todolist/config/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package org.example.todolist.config;
+
+import lombok.RequiredArgsConstructor;
+import org.example.todolist.domain.Member;
+import org.example.todolist.repository.MemberRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member member = memberRepository.findByLoginId(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+        return new CustomUserDetails(member);
+    }
+
+}
---
+++ src/main/java/org/example/todolist/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package org.example.todolist.repository;
+
+import org.example.todolist.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByLoginId(String loginId);
+
+}
