f24ad8b06558326054872d624ccbf307d9408eb5
Chan-GN <ocg0229@gmail.com>
2024-09-12 17:36:01 +0900

    REFACTOR: MemberController.getCurrentUser 최적화 - 로그인한 사용자의 정보를 반환하는 getCurrentUser 메서드를 userDetails를 통해 반환하도록 수정 - MemberService 제거

--- src/main/java/org/example/todolist/controller/MemberController.java
+++ src/main/java/org/example/todolist/controller/MemberController.java
@@ -1,27 +1,24 @@
 package org.example.todolist.controller;
 
-import lombok.RequiredArgsConstructor;
+import org.example.todolist.domain.Member;
 import org.example.todolist.dto.member.MemberResponseDto;
-import org.example.todolist.service.MemberService;
+import org.example.todolist.security.CustomUserDetails;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/api/member")
 public class MemberController {
 
-    private final MemberService memberService;
-
     @GetMapping
-    public ResponseEntity<MemberResponseDto> getCurrentUser(@AuthenticationPrincipal UserDetails userDetails) {
-        String loginId = userDetails.getUsername();
-        MemberResponseDto memberDto = memberService.getMemberByLoginId(loginId);
-        return ResponseEntity.ok(memberDto);
+    public ResponseEntity<MemberResponseDto> getCurrentUser(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        Member member = userDetails.getMember();
+        return ResponseEntity.ok(
+                new MemberResponseDto(member.getId(), member.getLoginId(), member.getName())
+        );
     }
 
 }
