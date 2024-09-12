9d375d6765adc4396eb7aaf1c8862e7e8b679bc4
Chan-GN <ocg0229@gmail.com>
2024-09-12 12:11:26 +0900

    FEAT: 전역 예외 처리기에 예외 핸들러 메서드 추가 - GlobalExceptionHandler에 커스텀 예외에 대한 메서드 추가

--- src/main/java/org/example/todolist/exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/todolist/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package org.example.todolist.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,6 +11,26 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ToDoNotFoundException.class)
     public ResponseEntity<String> handleToDoNotFoundException(ToDoNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<String> handleAccessDeniedException(AccessDeniedException ex) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Access Denied: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(DuplicateLoginIdException.class)
+    public ResponseEntity<String> handleDuplicateLoginIdException(DuplicateLoginIdException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<String> handleUnauthorizedException(UnauthorizedException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MemberNotFoundException.class)
+    public ResponseEntity<String> handleMemberNotFoundException(MemberNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
